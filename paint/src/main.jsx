a7818a006549a8bb4582c47db5986e182f9fbb35
oualid <azmioualid@gmail.com>
2024-07-02 19:28:54 -0500

    Installed tailwind, and created first component

--- paint/src/main.jsx
+++ paint/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {createRoot} from "react-dom/client";
+import Header from './components/Header'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Element = document.getElementById("root");
+const root = createRoot(Element);
+
+root.render
+(
+<Header/>
 )
+
