68f2e0d7c69e8b6dd23263b31811e8826c8c6769
Oazmi <oazmi@academic.rrc.ca>
2024-07-12 20:28:21 -0500

    Set up the backend correctly

--- paint/backend/server.js
+++ paint/backend/server.js
@@ -1,9 +1,24 @@
-const express = require("express");
+const express = require('express');
+const db = require('./db'); 
+
 const app = express();
+const port = 3000;
 
-app.get("/", (req, res) => {
-    console.log("here")
-    res.status(500).send("hi")
-})
+app.get('/', (req, res) => {
+  res.send('Hello World!'); 
+});
 
-app.listen(3000);
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).send('Error fetching users');
+      return;
+    }
+    console.log('Fetched users:', results); 
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
